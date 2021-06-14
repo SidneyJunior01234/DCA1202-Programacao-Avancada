bdb3db9ddbce7d3b38a0a4ea843e51e4b6e078fd
Sidney Alves <50020838+SidneyJunior01234@users.noreply.github.com>
2021-06-14 16:13:39 -0300

    Create Questao_8_a.c

---
+++ Ponteiros-C/Questão-8/Questao_8_a.c
@@ -0,0 +1,18 @@
+/*
+Instituicao: Universidade Federal do Rio Grande do Norte
+Disciplina: DCA1202 - Programacao Avancada
+Autor: Sidney Alves dos Santos Junior
+Descricao: O que faz o seguinte programas em C?
+*/
+#include <stdio.h>
+
+int main() 
+{
+  int vet[] = {4,9,13};//criado o vetor com tamanho 3
+  int i;//indice
+  for(i=0;i<3;i++)
+  {
+    printf("%d ",*(vet+i));//imprimir os valores do vetor de acordo com o indice
+  }
+  return 0;
+}
