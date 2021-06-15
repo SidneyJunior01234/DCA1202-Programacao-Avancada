245aa3d614da5f732d6fa17269a87e03f5262b52
Sidney Alves <50020838+SidneyJunior01234@users.noreply.github.com>
2021-06-14 21:27:01 -0300

    Update Questao_11.c

--- Ponteiros-C/Questão-11/Questao_11.c
+++ Ponteiros-C/Questão-11/Questao_11.c
@@ -15,7 +15,7 @@ int main()
     scanf("%f", &aloha);//nao eh valido, vetores sao referenciados e para usar o & aloha deveria ser um ponteiro
     
     //nao eh valido, aloha deve receber um valor float em uma posicao e nao uma string. Alem disso foi utilizado somente aspas duplas para abrir a string ocasionando em tudo que vier depois na linha se torne parte da string
-    aloha = value";
+    //aloha = value";
     printf("%f", aloha);//eh esperado que o valor impresso fosse um float nao um vetor float
     coisas[4][4] = aloha[3];//eh valido, coisas em uma posicao da matriz recebe um valor de uma posicao do vetor
     coisas[5] = aloha;//nao eh valido, o correto seria passar elemento por elemento em cada posicao 
