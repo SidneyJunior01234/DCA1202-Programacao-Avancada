0a91e0a86e7c30a738acbb780a33a0bed0dbb9a3
Sidney Alves <50020838+SidneyJunior01234@users.noreply.github.com>
2021-06-14 21:24:14 -0300

    Update Questao_9.c

--- Ponteiros-C/Questão-9/Questao_9.c
+++ Ponteiros-C/Questão-9/Questao_9.c
@@ -1,3 +1,17 @@
+/*
+Instituicao: Universidade Federal do Rio Grande do Norte
+Disciplina: DCA1202 - Programacao Avancada
+Autor: Sidney Alves dos Santos Junior
+Descricao: Seja x um vetor de 4 elementos, declarado da forma TIPO x[4];. Suponha que depois da
+declaração, x esteja armazenado no endereço de memória 4092 (ou seja, o endereço de x[0]).
+Suponha também que na máquina seja usada uma variável do tipo char ocupa 1 byte, do tipo
+int ocupa 2 bytes, do tipo float ocupa 4 bytes e do tipo double ocupa 8 bytes. Quais serão os
+valores de x+1, x+2 e x+3 se:
+◦ x for declarado como char?
+◦ x for declarado como int?
+◦ x for declarado como float?
+◦ x for declarado como double?
+*/
 #include <stdio.h>
 
 int main()
