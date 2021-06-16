4018b557e78c4cc73f636359d3e7187a5884f418
Sidney Alves <50020838+SidneyJunior01234@users.noreply.github.com>
2021-06-16 12:25:49 -0300

    Create Questão_13.c

---
+++ Ponteiros-C/Questão-13/Questão_13.c
@@ -0,0 +1,78 @@
+/*
+Instituicao: Universidade Federal do Rio Grande do Norte
+Disciplina: DCA1202 - Programacao Avancada
+Autor: Sidney Alves dos Santos Junior
+
+coletar_dados preenche o vetor com dados inseridos pelo usuario,
+tem como parametro um ponteiro que aponta para um vetor de inteiros e
+um inteiro referente ao tamanho do vetor.
+
+imprime_dados ira imprimir os valores armazenados no vetor,
+tem como parametro um ponteiro que aponta para um vetor de inteiros e
+um inteiro referente ao tamanho do vetor.
+
+ordenar_crescente eh responsavel por ordenar o vetor em ordem crescente,
+tem como parametro: dois ponteiros para funcoes do tipo void com parametros 
+inteiros sendo um deles ponteiro tambem. Um ponteiro para vetor do tipo inteiro
+e um inteiro referente ao tamanho do vetor.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void coletar_dados(int *vetor, int n)
+{
+  for (int i = 0; i < n; i++) 
+  {
+    printf("%d valor: ",(i+1));
+    scanf("%d",&vetor[i]);
+        
+  }
+  printf("\nvalores coletados\n");
+}
+
+void imprime_dados(int *vetor, int n)
+{
+  for (int i = 0; i < n; i++) 
+  {
+    printf("%d ",vetor[i]);
+  }
+}
+
+void ordenar_crescente(void (*f)(int*,int),int *vetor, int n, void (*i)(int*,int))
+{
+  int aux = 0;
+    
+  f(vetor,n);
+    
+  i(vetor,n);
+  
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      if(vetor[i] < vetor[j])
+      {
+        aux = vetor[i];
+        vetor[i] = vetor[j];
+        vetor[j] = aux;
+      }
+    }
+  }
+  printf("\nvalores ordenados\n");
+  i(vetor,n);
+}
+
+int main()
+{
+  int n, *vet;
+  printf("informe a quantidade de dados a serem alocados: ");
+  scanf("%d",&n);
+
+  vet = malloc(n * sizeof(int));//alocando o vetor
+    
+  ordenar_crescente(coletar_dados,vet,n,imprime_dados);
+
+  free(vet);
+  return 0;
+}
