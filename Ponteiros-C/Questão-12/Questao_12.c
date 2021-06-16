c8a0182bdb18ad6e71c5e216b56d40cc13857e7b
Sidney Alves <50020838+SidneyJunior01234@users.noreply.github.com>
2021-06-16 11:55:46 -0300

    Create Questao_12.c

---
+++ Ponteiros-C/Questão-12/Questao_12.c
@@ -0,0 +1,34 @@
+/*
+Instituicao: Universidade Federal do Rio Grande do Norte
+Disciplina: DCA1202 - Programacao Avancada
+Autor: Sidney Alves dos Santos Junior
+Descricao: exemplo de uso de ponteiros
+para funcoes
+*/
+#include <stdio.h>
+void soma(int a, int b)
+{
+  printf("soma = %d", (a+b));
+}
+
+void subtracao(int a, int b)
+{
+  printf("subtracao = %d", (a-b));
+}
+void multiplicacao(int a, int b)
+{
+  printf("multiplicacao = %d", (a*b));
+}
+
+void divisao(int a, int b)
+{
+  printf("divisao = %.2f", ((a*1.0)/b));
+}
+
+int main() 
+{
+  void (*pf_array[])(int,int) = {soma,subtracao,multiplicacao,divisao};
+  int opcao = 3, a = 4, b = 3;
+  (*pf_array[opcao])(a,b);
+  return 0;
+}
