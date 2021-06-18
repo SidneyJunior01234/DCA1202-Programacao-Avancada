1f87a6e3f8d54d7d8c243262bc4efcab6d630105
Sidney Alves <50020838+SidneyJunior01234@users.noreply.github.com>
2021-06-17 23:29:07 -0300

    Update Questao_14.c

--- Ponteiros-C/Questão-14/Questao_14.c
+++ Ponteiros-C/Questão-14/Questao_14.c
@@ -13,12 +13,12 @@ utilizando o qsort() e comentando os trechos de códigos.
  *tem como parâmetros: ponteiro para int e um inteiro.
  o ponteiro para int serve para usar o vetor alocado dinâmicamente.
 */
-void coletar_dados(int *vetor, int n)
+void coletar_dados(float *vetor, int n)
 {
   for (int i = 0; i < n; i++) 
   {
     printf("%d valor: ",(i+1));
-    scanf("%d",&vetor[i]);
+    scanf("%f",&vetor[i]);
         
   }
   printf("\nvalores coletados\n");
@@ -28,11 +28,11 @@ void coletar_dados(int *vetor, int n)
  *tem como parâmetros: ponteiro para int e um inteiro.
  *o ponteiro para int serve para usar o vetor alocado dinâmicamente.
 */
-void imprime_dados(int *vetor, int n)
+void imprime_dados(float *vetor, int n)
 {
   for (int i = 0; i < n; i++) 
   {
-    printf("%d ",vetor[i]);
+    printf("%.2f ",vetor[i]);
   }
 }
 /*ponteiro para função que compara dois elementos, responável por
@@ -40,20 +40,21 @@ void imprime_dados(int *vetor, int n)
 */
 int compare (const void * a, const void * b)
 {
-  return ( *(int*)a - *(int*)b );
+  return ( *(float*)a - *(float*)b );
 }
 
 int main()
 {
-  int n, *vet;
+  int n;
+  float *vet;
   printf("informe a quantidade de dados a serem alocados: ");
   scanf("%d",&n);
 
-  vet = malloc(n * sizeof(int));//alocando um vetor de n lementos
+  vet = malloc(n * sizeof(float));//alocando um vetor de n lementos
   
   coletar_dados(vet,n);//preenchendo o vetor
   imprime_dados(vet,n);//imprimindo o vetor de entrada
-  qsort(vet, n, sizeof(int), compare);//função de ordenação
+  qsort(vet, n, sizeof(float), compare);//função de ordenação
   printf("\nvalores ordenados\n");
   imprime_dados(vet,n);//imprimeos dados ordenados
   free(vet);//liberando a memoria alocada pelo vetor
