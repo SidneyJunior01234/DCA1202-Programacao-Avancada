f8b70024347d7e7d02ff42d1ad403a3a935f3117
Sidney Alves <50020838+SidneyJunior01234@users.noreply.github.com>
2021-06-24 12:11:25 -0300

    Update Questao_11.c

--- Ponteiros-C/Questão-11/Questao_11.c
+++ Ponteiros-C/Questão-11/Questao_11.c
@@ -1,7 +1,8 @@
 /*
 Instituicao: Universidade Federal do Rio Grande do Norte
 Disciplina: DCA1202 - Programacao Avancada
-Autor: Sidney Alves dos Santos Junior
+Autor(es): Ana Beatriz Fontes Ferreira
+           Sidney Alves dos Santos Junior
 Descricao: Identifique quais dos seguintes comandos é válido ou inválido.
 */
 #include <stdio.h>
