a2d111ad3b39ab83bf04e2b567213e5a39c3ee39
Sidney Alves <50020838+SidneyJunior01234@users.noreply.github.com>
2021-06-24 12:16:00 -0300

    Update Questao_6.c

--- Ponteiros-C/Questão-6/Questao_6.c
+++ Ponteiros-C/Questão-6/Questao_6.c
@@ -1,7 +1,8 @@
 /*
 Instituicao: Universidade Federal do Rio Grande do Norte
 Disciplina: DCA1202 - Programacao Avancada
-Autor: Sidney Alves dos Santos Junior
+Autor(es): Ana Beatriz Fontes Ferreira
+           Sidney Alves dos Santos Junior
 Descricao: Assumindo que pulo[] é um vetor do tipo int, quais das seguintes 
 expressões referenciam o
 valor do terceiro elemento do vetor?
