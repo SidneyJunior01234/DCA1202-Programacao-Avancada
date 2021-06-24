fb5c5f447f7cd156dd524b08456ee68d02f1ee15
Sidney Alves <50020838+SidneyJunior01234@users.noreply.github.com>
2021-06-24 12:12:59 -0300

    Update Questao_15.c

--- Ponteiros-C/Questão-15/Questao_15.c
+++ Ponteiros-C/Questão-15/Questao_15.c
@@ -1,7 +1,8 @@
 /*
 Instituicao: Universidade Federal do Rio Grande do Norte
 Disciplina: DCA1202 - Programacao Avancada
-Autor: Sidney Alves dos Santos Junior
+Autor(es): Ana Beatriz Fontes Ferreira
+           Sidney Alves dos Santos Junior
 Descricao: implementar uma funcao de ordenacao similar a qsort.
 */
 #include <stdio.h>
