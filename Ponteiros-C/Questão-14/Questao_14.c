33896f8a8d729455f384410d36a91fe0e0ba6137
Sidney Alves <50020838+SidneyJunior01234@users.noreply.github.com>
2021-06-24 12:12:45 -0300

    Update Questao_14.c

--- Ponteiros-C/Questão-14/Questao_14.c
+++ Ponteiros-C/Questão-14/Questao_14.c
@@ -1,7 +1,8 @@
 /*
 Instituicao: Universidade Federal do Rio Grande do Norte
 Disciplina: DCA1202 - Programacao Avancada
-Autor: Sidney Alves dos Santos Junior
+Autor(es): Ana Beatriz Fontes Ferreira
+           Sidney Alves dos Santos Junior
 Descricao: reescrever o programa da questao 13
 utilizando o qsort() e comentando os trechos de códigos.
 */
